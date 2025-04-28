f5722386790fc6bc4866daa189637b43f67d0d76
kharchenko_bohdan <bohdan.kharchenko@l-center.com>
2025-04-28 22:18:08 +0300

    lab3

--- lab2/app/src/main/java/com/example/lab2/InputFragment.kt
+++ lab2/app/src/main/java/com/example/lab2/InputFragment.kt
@@ -1,10 +1,12 @@
 package com.example.lab2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.lab2.databinding.FragmentInputBinding
@@ -30,14 +32,31 @@ class InputFragment : Fragment() {
                 val selectedFontId = binding.radioGroup.checkedRadioButtonId
                 val selectedFont = view.findViewById<RadioButton>(selectedFontId)?.text.toString()
 
+                val fullText = "Text: $text | Font: $selectedFont"
+
+                val success = StorageHelper.saveData(requireContext(), fullText)
+
+                if (success) {
+                    Toast.makeText(requireContext(), "Дані успішно збережені", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), "Помилка збереження даних", Toast.LENGTH_SHORT).show()
+                }
+
                 val action = InputFragmentDirections.actionInputFragmentToResultFragment(text, selectedFont)
                 findNavController().navigate(action)
             }
         }
+
         binding.btnCancel.setOnClickListener {
             binding.editText.text.clear()
         }
+
+        binding.btnOpen.setOnClickListener {
+            val intent = Intent(requireContext(), StorageActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
     fun clearInput() {
         binding.editText.text.clear()
